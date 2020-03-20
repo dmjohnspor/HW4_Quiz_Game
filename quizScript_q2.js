ff2959fe6e0d2efc4ce38bd05a7089fdab90a1bd
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-03-20 16:16:07 -0400

    Added js files for each one of the question pages

---
+++ quizScript_q2.js
@@ -0,0 +1,35 @@
+// Global scope variables for timer
+var timer = document.getElementById("timer");
+var secondsLeft = 75;
+timer.textContent = "Time left: " + secondsLeft;
+
+// Global scope variables for answer buttons
+var wrongBtns = document.getElementsByClassName("wrongBtn")
+var correctBtn = document.getElementById("correctBtn");
+var message = document.querySelector("#message");
+
+
+// Code for timer function
+setInterval(function () {
+    secondsLeft--;
+    timer.textContent = "Time left: " + secondsLeft;
+
+    if (secondsLeft <= 0) {
+        window.location.href = "6quizEnd.html";
+    }
+}, 1000)
+
+//Event listeners for the correct answer. Displays "correct" and redirects to next question.
+correctBtn.addEventListener("click", function () {
+    message.textContent = "Correct!!!";
+    setTimeout(function () { window.location.href = "4quizQuestion3.html" }, 1000);
+});
+
+//Event listener for the wrong answer. Displays "wrong" and subtracts 5 seconds off the timer.
+for (var i = 0; i < wrongBtns.length; i++) {
+    wrongBtns[i].addEventListener("click", function () {
+        secondsLeft -= 5;
+        message.textContent = "Wrong...";
+        setTimeout(function () { message.textContent = ""; }, 1000)
+    });
+}
--- quizScript_q4.js
+++ quizScript_q4.js
@@ -1,6 +1,6 @@
 // Global scope variables for timer
 var timer = document.getElementById("timer");
-var secondsLeft = 100000
+var secondsLeft = 75;
 timer.textContent = "Time left: " + secondsLeft;
 
 // Global scope variables for answer buttons
@@ -14,19 +14,21 @@ setInterval(function () {
     secondsLeft--;
     timer.textContent = "Time left: " + secondsLeft;
 
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
         window.location.href = "6quizEnd.html";
     }
 }, 1000)
 
-//Onclick event listeners to convey the messages
+//Event listeners for the correct answer. Displays "correct" and redirects to next question.
 correctBtn.addEventListener("click", function () {
     message.textContent = "Correct!!!";
-    setTimeout(function () { window.location.href = "3quizQuestion2.html" }, 1000);
+    setTimeout(function () { window.location.href = "6quizEnd.html" }, 1000);
 });
 
+//Event listener for the wrong answer. Displays "wrong" and subtracts 5 seconds off the timer.
 for (var i = 0; i < wrongBtns.length; i++) {
     wrongBtns[i].addEventListener("click", function () {
+        secondsLeft -= 5;
         message.textContent = "Wrong...";
         setTimeout(function () { message.textContent = ""; }, 1000)
     });
