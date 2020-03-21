7316343d4e1ae8bfa62d1645dd99912e342d233f
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-03-20 21:09:06 -0400

    Added score function on the quizEndScript.js file

--- quizEndScript.js
+++ quizEndScript.js
@@ -1,5 +1,15 @@
 // Global variables
 var submitBtn = document.getElementById("submitBtn");
+var score = localStorage.getItem("seconds");
+var timer = document.getElementById("timer");
+var finalScore = document.getElementById("finalScore");
+
+// Display how many seconds where left
+timer.textContent = "Time left: " + score;
+
+// Display final score
+finalScore.textContent = "Your final score: " + score;
+
 
 submitBtn.addEventListener("click", function () {
     var initials = document.getElementById("inputInitials").value;
