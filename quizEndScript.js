86f2e63fd5601a51f78ab8277da3350731b87488
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-03-23 13:43:18 -0400

    High score page not working properly

--- quizEndScript.js
+++ quizEndScript.js
@@ -1,22 +1,44 @@
 // Global variables
 var submitBtn = document.getElementById("submitBtn");
-var score = localStorage.getItem("seconds");
 var timer = document.getElementById("timer");
 var finalScore = document.getElementById("finalScore");
+var score = localStorage.getItem("seconds");
 
-// Display how many seconds where left
+
+// Display how many seconds are left
 timer.textContent = "Time left: " + score;
 
 // Display final score
 finalScore.textContent = "Your final score: " + score;
 
 
-submitBtn.addEventListener("click", function () {
+// Add new score to the stored scores list
+function addScore() {
     var initials = document.getElementById("inputInitials").value;
-    localStorage.setItem("initials", initials);
+    var scoreToStore = initials + " - " + score;
+    var scoreList = [];
+    scoreList.push(scoreToStore);
+    var highscores = localStorage.getItem("highscores");
 
-    if (initials === "") { alert("Please submit your initials") };
+    if (!highscores) { localStorage.setItem("highscores", scoreList) }
+    else {
+        scoreList.push(scoreToStore);
+        var newKey = JSON.stringify(scoreList);
+        localStorage.setItem("highscores", newKey);
+    }
+}
 
-    location.replace("7Highscores.html")
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    addScore();
+    location.replace("7Highscores.html");
 })
+
+
+
+
+
+
+
+
 
--- quizHighscoresScript.js
+++ quizHighscoresScript.js
@@ -2,9 +2,8 @@
 var backBtn = document.getElementById("backBtn");
 var clearBtn = document.getElementById("clearBtn");
 var highscoreList = document.getElementById("highscoreList");
-var score = localStorage.getItem("seconds");
-var initials = localStorage.getItem("initials");
-var storedScores = [];
+var li = document.createElement("li");
+
 
 // "Go back" button function
 backBtn.addEventListener("click", function () {
@@ -17,16 +16,16 @@ clearBtn.addEventListener("click", function () {
     localStorage.clear();
 })
 
-// // Adding highscores to localStorage
-var currentScore = initials + " - " + score;
-var storedScore = localStorage.setItem("highscore", currentScore);
+// Render highscore list
+function renderHighscores() {
+    var highscores = localStorage.getItem("highscores");
+    JSON.parse(highscores);
+    for (var i = 0; i < highscores.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = "😍 " + highscores[i];
+        li.setAttribute("class", "li");
+        highscoreList.appendChild(li);
+    }
+}
 
-// Adding highscores to the list
-var li = document.createElement("li");
-li.textContent = "😍 " + localStorage.getItem("highscore");
-highscoreList.appendChild(li);
-
-
-
-
-
+renderHighscores();
